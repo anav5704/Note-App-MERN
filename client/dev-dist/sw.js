5b2d3b766e206e59554960431c6ceee5f64d4bcd
Anav Chand <anavkki@gmail.com>
2023-09-23 14:45:46 +1200

    Refactored ✅

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.aho132sn5bg"
+    "revision": "0.r6qfot3rms8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
